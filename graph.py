a9449498f860e6e2f9132d1d8231f7e4ee4ced50
Sergio Gonzalez <Sergio.Gonzalez29@alu.uclm.es>
2018-09-24 19:34:16 +0200

    Definición clase Grafo

--- graph.py
+++ graph.py
@@ -1,1 +1,18 @@
-#BC1-01
+#!/usr/bin/python3
+#############################################################################
+##                              Sistemas Inteligentes                      ##
+##    Grupo: BC1 - 01                       Curso: 2018 / 2019             ##
+#############################################################################
+from networkx import *
+
+class Grafo:
+    def __init__(self): #Constructor
+        #read_graphml(path, node_type=<type 'str'>)
+
+    def perteneceNodo(self, idNodo):
+
+
+    def posicionNodo(self, idNodo):
+
+
+    def adyacentesNodo(self, idNodo):
