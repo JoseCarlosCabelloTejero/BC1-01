b3bbd1bde065565e0b4f3f34e70188c80e28b45e
Sergio Gonzalez <Sergio.Gonzalez29@alu.uclm.es>
2018-10-01 09:45:26 +0200

    Modificada clase principal.py. Variable con el nodo ejemplo

--- principal.py
+++ principal.py
@@ -7,20 +7,21 @@
 from graph import *
 
 grafo = Grafo("./Datos/data/Anchuras.graphml.xml")
+idNodo="946409139"
 
 ##### Ejemplo del petodo perteneceNodo
 
 print("Ejemplo del metodo perteneceNodo")
-print("Nodo: 4762868811 --> ",grafo.perteneceNodo("4762868811"))
+print("Nodo: ",idNodo," --> ",grafo.perteneceNodo(idNodo))
 print("Nodo 1234567890 --> ",grafo.perteneceNodo("1234567890"),'\n')
 
 #### Ejemplo del metodo posicionNodo
 print("Ejemplo del metodo posicionNodo")
-print("Altitud y longitud del nodo: 4762868811 -->",grafo.posicionNodo("4762868811"))
+print("Altitud y longitud del nodo:",idNodo," -->",grafo.posicionNodo(idNodo))
 print("Altitud y longitud del nodo que no existe -->",grafo.posicionNodo("1234567890"),'\n')
 #### Ejemplo de adyacentes nodos
 print("Ejemplo del metodo adyacentesNodo")
-print("Nodos adyacentes del nodo: 4762868811")
-aristas = grafo.adyacentesNodo("4762868811")
+print("Nodos adyacentes del nodo ",idNodo,":")
+aristas = grafo.adyacentesNodo(idNodo)
 for i in aristas:
     print(i)
