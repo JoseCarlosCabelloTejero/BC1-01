426011652052e0a4b04c1c6093074a1dd7d10fe0
DavidCarneros <David.Carneros1@alu.uclm.es>
2018-10-01 08:48:31 +0200

    Añadida en la clase principal ejemplos del funcionamiento de los metodos implementados en la clase grafo

--- principal.py
+++ principal.py
@@ -8,18 +8,19 @@ from graph import *
 
 grafo = Grafo("./Datos/data/Anchuras.graphml.xml")
 
-#print(list(grafo.getGraph()))
-#print(len(list(grafo.getGraph())))
+##### Ejemplo del petodo perteneceNodo
 
-#print(grafo.perteneceNodo('4762868811'))
+print("Ejemplo del metodo perteneceNodo")
+print("Nodo: 4762868811 --> ",grafo.perteneceNodo("4762868811"))
+print("Nodo 1234567890 --> ",grafo.perteneceNodo("1234567890"),'\n')
 
-
-#print(grafo.posicionNodo('4762868811'))
-
-#print(grafo.posicionNodo('4762868811'))
-
-
-aristas = grafo.adyacentesNodo("946409156")
-
+#### Ejemplo del metodo posicionNodo
+print("Ejemplo del metodo posicionNodo")
+print("Altitud y longitud del nodo: 4762868811 -->",grafo.posicionNodo("4762868811"))
+print("Altitud y longitud del nodo que no existe -->",grafo.posicionNodo("1234567890"),'\n')
+#### Ejemplo de adyacentes nodos
+print("Ejemplo del metodo adyacentesNodo")
+print("Nodos adyacentes del nodo: 4762868811")
+aristas = grafo.adyacentesNodo("4762868811")
 for i in aristas:
     print(i)
