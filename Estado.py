aad25cd2fcfd96da0916174309835ca2450991e4
Andres Gutierrez <Andres.Gutierrez@alu.uclm.es>
2018-10-08 18:26:02 +0200

    Clase Estado implementada

--- Estado.py
+++ Estado.py
@@ -8,18 +8,21 @@ import hashlib
 
 class Estado:
 
-    def md5(self,id,lista):
+    def md5metodo(self,id,lista):
+        n=0
         m = hashlib.md5()
-        m.update(id)
-        m.update(lista)
+        m.update(id.encode())
+        for i in lista:
+            m.update(lista[n].encode())
+            n=n+1
         return m.hexdigest()
 
     def __init__(self):
-        self.node = 4331489739
+        self.node = "4331489739"
         self.listNodes =["4331489528","4331489668","4331489711","4762868815","4928063625"]
-        self.id =  self.md5(self.node,self.listNodes)
+        self.id = self.md5metodo(self.node,self.listNodes)
 
 
 
-    def prueba():
+    def prueba(self):
         print(self.id)
