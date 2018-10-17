3cc229b9db4265b800432ae6f280fa0236874163
Sergio Gonzalez <Sergio.Gonzalez29@alu.uclm.es>
2018-10-17 19:55:28 +0200

    Terminada clase Frontera

--- Frontera.py
+++ Frontera.py
@@ -3,22 +3,23 @@
 ##                       Sistemas Inteligentes                             ##
 ##    Grupo: BC1 - 01                       Curso: 2018 / 2019             ##
 #############################################################################
-
+from Nodo import Nodo
 
 class Frontera:
 
     def __init__(self):
-        self.listaFrontera=[]
+        self.listaFrontera=CreaFrontera()
 
     def CreaFrontera(self):
-
+        frontera = []
+        return frontera
 
     def insertar(self,nodoArbol):
         self.listaFrontera.append(nodoArbol)
         self.listaFrontera=sorted(self.listaFrontera,key=lambda Nodo: Nodo.getF())
 
     def elimina(self):
-        
+
         return self.listaFrontera.pop(0)
 
 
